1a66e370c4d191664ad250feaf5bdcb51bdd7e71
KorolDvijkov <153953905+KorolDvijkov@users.noreply.github.com>
2025-01-09 15:11:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+ 
+root = Tk()
+root.title("Masterclub")
+root.geometry("250x200")
+ 
+ttk.Entry().pack(anchor=NW, padx=7, pady= 7)
+ 
+root.mainloop()
 
+  
